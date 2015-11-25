0609c3883e697a6b0cdf3db9d52730d02ade9191
nancymi <13121178nancy@gmail.com>
2015-11-25 09:09:00 +0800

    Resolve: define function called 'even_group_calculate_average' *Layout Problem - Cannot resolve*

--- src/calculate-third.js
+++ src/calculate-third.js
@@ -1,3 +1,47 @@
 function even_group_calculate_average(arr) {
-  
+  arr.getSubArray(2).getEven().getGroupByDigit().getGroupAverage();
+}
+
+Array.prototype.getEven = function() {
+  var evens = new Array();
+  for(var i = 0; i < this.length; i ++) {
+    if (this[i]%2 == 0)
+      evens.push(this[i]);
+  }
+  return evens;
+}
+
+Array.prototype.getGroupByDigit = function() {
+  var evensByDigit = new Array();
+  for (var i = 0; i < this.length; i ++) {
+    var digit = this[i].toString().length;
+    if (evensByDigit[digit] == null)
+      evensByDigit[digit] = new Array();
+    evensByDigit[digit].push(this[i]);
+  }
+  return evensByDigit;
+}
+
+Array.prototype.getGroupAverage = function() {
+  var averages = new Array();
+  if (this.length == 0) {
+    alert(averages);
+    return averages;
+  }
+  for (var i = 0; i < this.length; i ++) {
+    if (this[i] != null) {
+      var average = getAve(this[i]);
+      averages.push(average);
+    }
+  }
+  return averages;
+}
+
+function getAve(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i ++) {
+    sum += arr[i];
+  }
+  sum /= arr.length;
+  return parseInt(sum);
 }
